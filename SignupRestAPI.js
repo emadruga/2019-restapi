c2a2ed51efecfe85f9d3dd8dccb5d9ba4750379f
Ewerton Madruga <emadruga@gmail.com>
2019-08-29 19:31:17 -0300

    - Added a signup ID (matricula) to every new person inserted into the user base.

--- SignupRestAPI.js
+++ SignupRestAPI.js
@@ -14,6 +14,12 @@ var whitelist = ['https://stark-caverns-59860.herokuapp.com','http://localhost:8
 
 var CAPTCHA_KEY = process.env.SECRET_KEY;
 
+// Used to create IDs for newly added people
+var nextAvailableID = 0;
+
+// Appended to candidate ID to comput verification digit
+var YEAR_OF_SIGNUP = "2019";
+
 console.log("Database connection: " + process.env.MONGODB_URI);
 
 var corsOptions = {
@@ -43,6 +49,7 @@ mongoose.connection.on("error", function(err) {
     process.exit();
 });
 
+
 app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
 app.use(logger('combined')); // Log requests to API using morgan
@@ -64,13 +71,61 @@ var Room = mongoose.model('Room', {
     escola_publica: String,
     deficiencia:    String,
     cotista:        String,
-    renda:        String
+    ppi:            String,  // nao/pre/par/ind
+    renda:          String,  // acima/abaixo
+    modo_pagam:     String,  // gru/isencao
+    doc_entregue:   String   // sim/nao
+    
 });
 
-/*
- * Generate some test data, if no records exist already
- * MAKE SURE TO REMOVE THIS IN PROD ENVIRONMENT
-*/
+
+// ----------------------------------------------
+// Calcula o dígito em Módulo 10 do número dado.
+// Os valores de entrada e saída são string.
+// ----------------------------------------------
+function CalculaDigitoMod10(Dado)
+{
+/*--
+Banco Itau usa Mod10 para agencia/ conta bancaria
+
+Exemplo: Agência 0155 Conta 50566-2
+ +---+---+---+---+---+---+---+---+---+   +---+
+ | 0 | 1 | 5 | 5 | 5 | 0 | 5 | 6 | 6 | - | 2 |
+ +---+---+---+---+---+---+---+---+---+   +---+
+    |   |   |   |   |   |   |   |   |
+   x2  x1  x2  x1  x2  x1  x2  x1  x2
+    |   |   |   |   |   |   |   |   | (soma digitos)
+   =0  =1 =10  =5 =10  =0 =10  =6 =12 ("12" = 1+2 = 3)
+    +---+---+---+---+---+---+---+---+-> = 18
+  (18 / 10) = 1, resto 8 => DV = (10 - 8) = 2
+--*/
+    var i;
+    var mult = 2;
+    var soma = 0;
+    var s = "";
+
+
+    Dado = YEAR_OF_SIGNUP + Dado;
+    
+    for (i=Dado.length-1; i>=0; i--)
+    {
+	s = (mult * parseInt(Dado.charAt(i))) + s;
+	if (--mult<1)
+	{
+	    mult = 2;
+	}
+    }
+    for (i=0; i<s.length; i++)
+    {
+	soma = soma + parseInt(s.charAt(i));
+    }
+    soma = soma % 10;
+    if (soma != 0)
+    {
+	soma = 10 - soma;
+    }
+    return soma.toString();
+}
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -85,6 +140,73 @@ function getLastFourDigits(cpf) {
     response = response.substr(response.length-4);
 
     return response;
+}
+
+function pad(num_str, size) {
+    // pad zeroes up to 'size' digits. Ex: pad(99,4) -> '0099'
+    return ('0000000000' + num_str).substr(-size);
+}
+
+function trimGroupAndDv(id) {
+    var response = id.replace(/\-/g, '');
+    response = response.replace(/\./g, '');
+
+    // trim signup group (cotas) info (first digit)
+    response = response.substring(1, response.length);
+
+    // trim DV (last digit)
+    response = response.substring(0, response.length - 1);
+    
+    return response;
+}
+
+function getLastID() {
+    var result = 0;
+    
+    Room.findOne({}, {}, { sort: { 'data_criado' : -1 } }, (err, user) => {
+        if (err) {
+            // handle error
+            result =  0;
+        }
+
+        if (user) {
+            // this is user last inserted
+	    var last_id = trimGroupAndDv(user.rg_identidade);
+	    last_id = getLastFourDigits(last_id);
+            console.log("Last ID: " + last_id);
+
+	    result =  Number(last_id);
+        } else{
+            console.log("No user found. Empty db ?.");
+            result = 0;
+	}
+    });
+
+}
+
+function getHighDigit(cotista, ppi, renda) {
+    var digit = 9;
+    if (cotista && cotista === 'nao') {
+	// cotista defined and not a cotista
+	digit = 5;
+    } else if (cotista) {
+	// it is a cotista candidate
+	if (renda && renda === 'abaixo') {
+	    if (ppi && ppi !== 'nao') {
+		digit = 1; 
+	    } else if (ppi) {
+		digit = 2;
+	    }
+	} else if (renda) {
+	    // renda = acima
+	    if (ppi && ppi !== 'nao') {
+		digit = 3; 
+	    } else if (ppi) {
+		digit = 4;
+	    }
+	}
+    }
+    return digit;
 }
 
 //Room.remove({}, function(res){
@@ -182,7 +304,32 @@ app.post('/api/rooms/insert', cors(corsOptions), function(req, res, next) {
     const cpf_info = req.body.cpf;
     var client_addr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
+    Room.findOne({}, {}, { sort: { 'data_criado' : -1 } }, (err, user) => {
+        if (err) {
+            // handle error
+            console.log("Problema no servidor: tente de novo mais tarde...");
+            err.httpStatusCode = 500
+            return next(err)
+        }
+
+        if (user) {
+            // this is user last inserted
+	    var last_id = trimGroupAndDv(user.rg_identidade);
+	    last_id = getLastFourDigits(last_id);
+            console.log("Last ID: " + last_id);
+
+	    nextAvailableID =  Number(last_id);
+	    
+        } else{
+	    
+            console.log("No user found. Empty db ?.");
+            nextAvailableID = 0;
+	}
+    });
+
     console.log("Insert request from: " + client_addr);
+    console.log("            next ID: " + nextAvailableID);
+
     if (!cpf_info) {
         const error = new Error('Faltando CPF no registro...')
         error.httpStatusCode = 400
@@ -209,30 +356,55 @@ app.post('/api/rooms/insert', cors(corsOptions), function(req, res, next) {
             // proceed with creating user...
             console.log("Inserting: " + req.body.cpf );
 
+	    // what group candidate belongs to?
+	    var group = getHighDigit(req.body.cotista,
+				     req.body.ppi,
+				     req.body.renda);
+	    
+	    // Assign an ID to candidate
+	    var nextID_num = nextAvailableID + 1;
+	    nextAvailableID = nextAvailableID + 1;
+	    var nextID = pad(nextID_num.toString(), 4);
+            console.log("nextID1: " + nextID );
+	    nextID = group.toString() + nextID;
+            console.log("nextID2: " + nextID );
+	    var dv = CalculaDigitoMod10(nextID);
+	    nextID = nextID + "-" + dv;
+            console.log("dv: " + dv );
+
+
             var newPerson = new Room({
                 nome_completo:  req.body.nome_completo,
                 data_nasc:      req.body.data_nasc,
                 data_criado:    moment().format(),
-                rg_identidade:  req.body.rg_identidade,
-                cpf:	           req.body.cpf,
-                sexo:	   req.body.sexo,
-                email:	   req.body.email,
-                cidade:	   req.body.cidade,
-                cep:	           req.body.cep,
-                telefone:	   req.body.telefone,
+                // rg_identidade:  req.body.rg_identidade,
+                rg_identidade:  nextID,
+                cpf:	        req.body.cpf,
+                sexo:	        req.body.sexo,
+                email:	        req.body.email,
+                cidade:	        req.body.cidade,
+                cep:	        req.body.cep,
+                telefone:	req.body.telefone,
                 deficiencia:    req.body.deficiencia,
                 escola_publica: req.body.escola_publica,
-                cotista:        req.body.cotista,
-                renda:        req.body.renda
+                cotista:        req.body.cotista,       // sim/nao
+                renda:          req.body.renda,         // acima/abaixo
+		ppi:            req.body.ppi,           // nao/pre/par/ind
+		modo_pagam:     req.body.modo_pagam,    // gru/isencao
+		doc_entregue:   req.body.doc_entregue   // sim/nao
             });
 
+	    
             newPerson.save(function(err, doc){
                 if (err) {
                     console.log("Server Insert Error: " + err);
                     res.send(err);
                 }
 
-                console.log("Created person: " + doc.cpf);
+                console.log("Created person: " + doc.nome_completo);
+                console.log("           cpf: " + doc.cpf);
+                console.log("         email: " + doc.email);
+                console.log("     matricula: " + doc.rg_identidade);
                 res.json(doc);
             });
         }
@@ -267,8 +439,11 @@ app.put('/api/rooms/update', cors(corsOptions), function(req, res, next) {
         telefone:	req.body.telefone,
         deficiencia:    req.body.deficiencia,
         escola_publica: req.body.escola_publica,
-        cotista:        req.body.cotista,
-        renda:        req.body.renda
+        cotista:        req.body.cotista,       // sim/nao
+        renda:          req.body.renda,         // acima/abaixo
+	ppi:            req.body.ppi,           // nao/pre/par/ind
+	modo_pagam:     req.body.modo_pagam,    // gru/isencao
+	doc_entregue:   req.body.doc_entregue   // sim/nao
     };
 
     var query = { cpf: cpf_info };
